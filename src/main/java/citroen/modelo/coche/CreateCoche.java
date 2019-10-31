07369725de23ccd2fa1df4212d483c3f1d3e8638
bochiclase <macbookair@192.168.8.104>
2019-10-31 02:14:56 +0100

    Primer commit

--- src/main/java/citroen/modelo/coche/CreateCoche.java
+++ src/main/java/citroen/modelo/coche/CreateCoche.java
@@ -12,7 +12,7 @@ public void create(CocheBean coche) {
 		
 		// Recoge la conexi�n
 		EntityManager entityManager = ConnectionEntityManagerFactory.getEntityManager().createEntityManager();
-		
+		 
 		// empieza la transacci�n, persistimos y cerramos
 		entityManager.getTransaction().begin();
 		entityManager.persist(coche);
